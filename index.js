a3c09789e42999106b76ca70d652b9b56a2d3ab9
Bríd O'Donnell <odonneb4@tcd.ie>
2023-02-08 12:33:13 +0000

    Fixed the port

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 app.use(`${api}`, dataRoute);
 
-app.listen(process.env.PORT || 8000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Responder API is running!");
   console.log(`Server started on PORT: ${process.env.PORT}`);
 });
