8be6682d8867619a31edfd5f9514fb22b33c7e16
Bríd O'Donnell <43962512+bod777@users.noreply.github.com>
2023-02-15 14:05:40 +0000

    Removed unnecessary env variables

--- index.js
+++ index.js
@@ -2,27 +2,14 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 
 const dataRoute = require("./routes/data");
 
-const api = process.env.API_URI;
-
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connection Successfull!"))
-  .catch((err) => console.log(err));
-
-mongoose.connection.on("disconnected", () =>
-  console.log("mongoDB disconnected!")
-);
-mongoose.connection.on("connected", () => console.log("mongoDB connected!"));
-
 app.use(cors());
 app.use(express.json());
-app.use(`${api}`, dataRoute);
+app.use('/api/v1',dataRoute);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(5000 || 5000, () => {
   console.log("Responder API is running!");
-  console.log(`Server started on PORT: ${process.env.PORT}`);
+  console.log(`Server started on PORT: ${5000}`);
 });
